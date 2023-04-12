4eaffbdeb0dbca97e0a383579b661df3b8aa5355
gilbertotv <gilcarrillo7@gmail.com>
2023-04-12 00:44:12 -0600

    form select

--- src/components/shared/Select.tsx
+++ src/components/shared/Select.tsx
@@ -9,7 +9,7 @@ const Select = ({
 }) => {
 	return (
 		<select className="w-full h-[60px] sm:h-[72px] p-4 sm:p-6 border-2 border-gray3 rounded-lg text-gray1 text-base sm:text-lg mb-4 sm:mb-8 opacity-60">
-			<option className="" disabled value="">
+			<option className="" value="">
 				{placeholder}
 			</option>
 			{children}

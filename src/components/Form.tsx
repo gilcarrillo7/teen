691d960b7faac7401cb27a8fd94e3b0731b0d957
gilbertotv <gilcarrillo7@gmail.com>
2023-04-12 01:00:00 -0600

    form succes

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,14 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "./shared/Button";
 import Input from "./shared/Input";
 import Select from "./shared/Select";
+import { BeatLoader } from "react-spinners";
 const Form = () => {
+	const [status, setStatus] = useState<"pending" | "sending" | "sent">(
+		"pending"
+	);
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		setStatus("sending");
+		setTimeout(() => setStatus("sent"), 1500);
+	};
 	return (
 		<div className="bg-white mx-auto w-full lg:w-[800px] shadow-xl text-center p-4 sm:p-12 rounded-2xl">
 			<p className="font-walsheimpro text-2xl sm:text-4xl mb-8">
 				Let's Schedule a call and work together
 			</p>
-			<form className="w-full">
+			<form className="w-full" onSubmit={(e) => handleSubmit(e)}>
 				<Input placeholder="Full name" />
 				<div className="flex flex-col sm:flex-row sm:gap-6">
 					<Input placeholder="Phone" type="number" />
@@ -24,7 +34,13 @@ const Form = () => {
 					<option>Three</option>
 				</Select>
 				<div className="flex justify-end">
-					<Button text="Send inquiry" type="submit" />
+					{status === "sent" && (
+						<p className="font-walsheimpro text-blue1 text-xl sm:text-2xl">
+							Thanks, we will get back to you soon.{" "}
+						</p>
+					)}
+					{status === "sending" && <BeatLoader color="#191565" />}
+					{status === "pending" && <Button text="Send inquiry" type="submit" />}
 				</div>
 			</form>
 		</div>
